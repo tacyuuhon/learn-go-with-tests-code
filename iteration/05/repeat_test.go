5efb437f6721807332277b923af39632d75eaefe
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-14 14:51:49 +0800

    iteration add 05 case

---
+++ iteration/05/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a")
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
