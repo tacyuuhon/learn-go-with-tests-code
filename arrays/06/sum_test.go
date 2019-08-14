c3a86d47dcb142dd524ae4e6ffe5f23ae38517bc
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-14 17:48:54 +0800

    change arrays 06 testcase

--- arrays/06/sum_test.go
+++ arrays/06/sum_test.go
@@ -58,14 +58,44 @@ func TestSumAllTails(t *testing.T) {
 	})
 }
 
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum([]int{1, 2, 3, 4, 5})
+	}
+}
+
 func BenchmarkSumAll(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		SumAll([]int{1, 2}, []int{0, 9})
 	}
 }
 
-func ExampleSumAll() {
-	sum := SumAll([]int{1, 2}, []int{0, 9})
+func BenchmarkSumAllTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAllTails([]int{1, 2}, []int{0, 9}, []int{5, 8, 10})
+	}
+}
+
+func ExampleSum() {
+	sum := Sum([]int{1, 2, 3, 4, 5})
 	fmt.Println(sum)
+	// Output: 15
+}
+
+func ExampleSumAll_case1() {
+	sum1 := SumAll([]int{1, 2}, []int{0, 9})
+	fmt.Println(sum1)
 	// Output: [3 9]
+}
+
+func ExampleSumAll_case2() {
+	sum2 := SumAll([]int{}, []int{0, 9})
+	fmt.Println(sum2)
+	// Output: [0 9]
+}
+
+func ExampleSumAllTails() {
+	sum := SumAllTails([]int{1, 2}, []int{0, 9}, []int{5, 8, 10})
+	fmt.Println(sum)
+	// Output: [2 9 18]
 }
