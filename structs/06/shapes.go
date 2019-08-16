36596280dd33f6cb9d80b8c40315a2861b7d0b9e
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-16 15:30:53 +0800

    structs add 06 case

---
+++ structs/06/shapes.go
@@ -0,0 +1,41 @@
+package structs
+
+import "math"
+
+// Rectangle struct
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Area method
+// Calc Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle struct
+type Circle struct {
+	Radius float64
+}
+
+// Area method
+// Calc Circle
+func (c Circle) Area() float64 {
+	return (c.Radius * c.Radius) * math.Pi
+}
+
+// Shape interface
+type Shape interface {
+	Area() float64
+}
+
+// Premeter func
+func Premeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// Area func
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
