dd8ead79747f41095a73acefaed8ea0e81578275
tacyuuhon <tacyuuhon@gmail.com>
2019-08-20 02:02:33 +0800

    dictionary add 02 case

---
+++ maps/02/dictionary_test.go
@@ -0,0 +1,18 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
