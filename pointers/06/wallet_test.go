0d58e5a0fbc68dc0cddab7d89090853920b3192f
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-19 18:18:24 +0800

    add pointers 06 case

---
+++ pointers/06/wallet_test.go
@@ -0,0 +1,29 @@
+package pointers
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t *testing.T, wall Wallet, want Bitcoin) {
+		got := wall.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+}
