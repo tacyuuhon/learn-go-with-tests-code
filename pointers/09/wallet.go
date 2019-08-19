a82d8c636b2a53abeda99062250b1e1c374613e3
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-19 18:45:38 +0800

    add pointers 09 case

---
+++ pointers/09/wallet.go
@@ -0,0 +1,42 @@
+package pointers
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Bitcoin type
+type Bitcoin int
+
+// Wallet struct
+type Wallet struct {
+	balance Bitcoin
+}
+
+// Deposit func.
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Println("ToString of balance in Deposit is", &w.balance)
+	w.balance += amount
+}
+
+// Balance func
+func (w *Wallet) Balance() Bitcoin {
+	fmt.Println("ToString of balance in Balance is", &w.balance)
+	return w.balance
+}
+
+// Withdraw func
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+// Stirng func over fmt.Stringer method
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
