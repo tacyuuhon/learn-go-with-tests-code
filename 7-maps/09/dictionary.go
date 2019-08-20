58d78682dc96287107bb5b5386347a579dff1359
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-20 17:38:04 +0800

    dictionary add 09 case

---
+++ 7-maps/09/dictionary.go
@@ -0,0 +1,62 @@
+package dictionary
+
+// Dictionary struct
+type Dictionary map[string]string
+
+// DictionaryErr error
+type DictionaryErr string
+
+const (
+	// ErrNotFound error
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+	// ErrWordExists error
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	// ErrWordDoesNotExist error
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+// Search func
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+// Add func
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// Update func
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
