424d9ab86268adc3645c15adc42b1cc3f04eadbb
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-20 13:46:42 +0800

    dictionary add 04 case

---
+++ 7-maps/04/dictionary.go
@@ -0,0 +1,21 @@
+package dictionary
+
+import (
+	"errors"
+)
+
+// Dictionary struct
+type Dictionary map[string]string
+
+// ErrNotFound error
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+// Search func
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
