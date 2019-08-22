35fd34f70b2f48a309ee8dcb189283f574f0482a
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-22 19:03:58 +0800

    mocking add v06 case

---
+++ learn9-mocking/v06/countdown.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func main() {
+	Countdown(os.Stdout)
+}
+
+// Countdown func
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
+}
