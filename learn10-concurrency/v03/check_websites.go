6a72ce01f3053e23c6ea1e37f1aaeb1638e89c3f
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-23 17:45:53 +0800

    concurrency change v03 case

--- learn10-concurrency/v03/check_websites.go
+++ learn10-concurrency/v03/check_websites.go
@@ -1,23 +1,29 @@
 package concurrency
 
-import (
-	"time"
-)
-
 // WebsiteChecker func
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 // CheckWebsites fuc
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			// send statement
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		// receive expression
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
 	return results
 }
