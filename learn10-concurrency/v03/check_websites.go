ca37e17aa9c2f38bfd3f6b8a688b36e4b5d6cd1d
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-23 17:34:10 +0800

    concurrency add v03 case

---
+++ learn10-concurrency/v03/check_websites.go
@@ -0,0 +1,23 @@
+package concurrency
+
+import (
+	"time"
+)
+
+// WebsiteChecker func
+type WebsiteChecker func(string) bool
+
+// CheckWebsites fuc
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	return results
+}
