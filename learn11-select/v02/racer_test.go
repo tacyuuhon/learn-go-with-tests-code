1a040139235daba8fbed3886e478441434f17e2a
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-26 11:06:13 +0800

    select add v02 case

---
+++ learn11-select/v02/racer_test.go
@@ -0,0 +1,36 @@
+package racer
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	fmt.Printf("slowURL '%s', fastURL '%s'", slowURL, fastURL)
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
