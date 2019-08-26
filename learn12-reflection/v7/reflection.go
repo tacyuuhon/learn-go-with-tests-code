4b3422f4fadc4bd20b415ff8bb74a374f6b22bbc
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-26 17:41:55 +0800

    reflection add v7 case

---
+++ learn12-reflection/v7/reflection.go
@@ -0,0 +1,22 @@
+package reflection
+
+import (
+	"reflect"
+)
+
+// Walk func
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
+		}
+	}
+}
