9cca795c2c49e65ae48379c2f48053a09ac25dca
tian-zhongbao <tian_zhongbao@linecorp.com>
2019-08-26 17:21:17 +0800

    reflection add v5 case

---
+++ learn12-reflection/v5/reflection.go
@@ -0,0 +1,18 @@
+package reflection
+
+import (
+	"reflect"
+)
+
+// Walk func
+func walk(x interface{}, fn func(string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
+}
