989293ddde97c6d52112f5ddc4e6166f231340f4
TIAN ZHONGBAO <JP26142@w022581902977m.local>
2019-09-09 19:12:47 +0900

    io add v2 case

--- learn15-io/v2/FileSystemStore.go
+++ learn15-io/v2/FileSystemStore.go
@@ -4,11 +4,25 @@ import "io"
 
 // FileSystemStore struct
 type FileSystemStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 // GetLeague func of FileSystemStore
 func (f *FileSystemStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
+}
+
+// GetPlayerScore func
+func (f *FileSystemStore) GetPlayerScore(name string) int {
+	var wins int
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
 }
--- learn15-io/v2/FileSystemStore_test.go
+++ learn15-io/v2/FileSystemStore_test.go
@@ -22,5 +22,23 @@ func TestFileSystemStore(t *testing.T) {
 		}
 
 		assertLeague(t, got, want)
+
+		got = store.GetLeague()
+		assertLeague(t, got, want)
+	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo","Wins": 10},
+			{"Name": "Chris","Wins": 33}]`)
+
+		store := FileSystemStore{database}
+
+		got := store.GetPlayerScore("Chris")
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
 	})
 }
