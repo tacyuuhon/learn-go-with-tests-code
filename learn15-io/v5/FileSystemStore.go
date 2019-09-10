975e5d34ef2f8f73b390364ec1602f75484cb06f
TIAN ZHONGBAO <JP26142@w022581902977m.local>
2019-09-10 11:11:39 +0900

    io add v5 case

---
+++ learn15-io/v5/FileSystemStore.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// FileSystemStore struct
+type FileSystemStore struct {
+	database io.ReadWriteSeeker
+}
+
+// GetLeague func of FileSystemStore
+func (f *FileSystemStore) GetLeague() League {
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
+	return league
+}
+
+// GetPlayerScore func
+func (f *FileSystemStore) GetPlayerScore(name string) int {
+	var wins int
+	player := f.GetLeague().Find(name)
+
+	if player != nil {
+		wins = player.Wins
+	}
+
+	return wins
+}
+
+// RecordWin func
+func (f *FileSystemStore) RecordWin(name string) {
+	league := f.GetLeague()
+	player := league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	}
+
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(league)
+}
